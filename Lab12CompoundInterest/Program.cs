4dff54f197f0581736f311724f5ece4bd07a9820
bfleury <bfleury@outlook.com>
2017-10-12 17:51:19 -0400

    final

--- Lab12CompoundInterest/Program.cs
+++ Lab12CompoundInterest/Program.cs
@@ -12,29 +12,32 @@ namespace Lab12CompoundInterest
         {
 
             Console.Write("Enter principal amount: ");
-            decimal amount = decimal.Parse(Console.ReadLine());
-
-            Console.Write("Enter time in years investment: ");
             decimal principal = decimal.Parse(Console.ReadLine());
 
+            Console.Write("Enter interest rate: ");
+            double interestRate = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter time in years investment: ");
+            int nyears = int.Parse(Console.ReadLine());
+
             Console.Write("Enter number of times per year\nthe interest is compounded [2,4 or 12]: ");
-            decimal rate = decimal.Parse(Console.ReadLine());
+            int numTimes = int.Parse(Console.ReadLine());
 
+            Console.WriteLine();
 
-            for (int rate = 5; rate <= 10; rate++)
+            for (double rate = interestRate; rate <= 6.25; rate+=0.25)
             {
-                double interestRate = rate / 100.00;
-
                 Console.WriteLine($"Interest Rate: {rate:F}% on amount {principal:C}");
-                Console.WriteLine("Year   Amount on deposit");
+                Console.WriteLine("Year   Current balance");
 
-
-                for (int year = 1; year <= 10; ++year)
+                for (int year = 1; year <= nyears; year++)
                 {
-                    decimal amount = principal * (decimal)Math.Pow(1.0 + interestRate, year);
+                    decimal amount = principal * (decimal)Math.Pow((1 + ((rate / 100) / numTimes)), (numTimes * year));
                     Console.WriteLine($"{year,4}{amount,20:C}");
                 }
+
                 Console.WriteLine();
+
             }
         }
     }
